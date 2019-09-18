2529fc5956dc081278092275a2e1cb89314e20a4
Teis <teisgossen@gmail.com>
2019-09-18 11:28:05 +0200

    Slope Movement
    +Can move up a slope
    +Check collision above and front of the player while moving on the slope

--- Controller_2D.cs
+++ Controller_2D.cs
@@ -11,6 +11,8 @@ public class Controller_2D : MonoBehaviour {
     public int horizontalRayCount = 4;
     public int verticalRayCount   = 4;
 
+    float maxClimbAngle = 80;
+
     float horizontalRaySpacing;
     float verticalRaySpacing;
 
@@ -53,11 +55,30 @@ public class Controller_2D : MonoBehaviour {
             Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
 
             if (hit) {
-                velocity.x = (hit.distance - skinWidth) * directionX;
-                rayLength = hit.distance;
 
-                collisions.left  = directionX == -1;
-                collisions.right = directionX ==  1;
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+                if (i == 0 && slopeAngle <= maxClimbAngle) {
+                    float distanceToSlopeStart = 0;
+                    if (slopeAngle != collisions.slopeAngleOld) {
+                        distanceToSlopeStart = hit.distance - skinWidth;
+                        velocity.x -= distanceToSlopeStart * directionX;
+                    }
+                    ClimbSlope(ref velocity, slopeAngle);
+                    velocity.x += distanceToSlopeStart * directionX;
+                }
+
+                if (!collisions.climbingSlope || slopeAngle > maxClimbAngle) {
+                    velocity.x = (hit.distance - skinWidth) * directionX;
+                    rayLength = hit.distance;
+
+                    if (collisions.climbingSlope) {
+                        velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x); ;
+                    }
+
+                    collisions.left  = directionX == -1;
+                    collisions.right = directionX ==  1;
+                }
             }
         }
     }
@@ -77,9 +98,26 @@ public class Controller_2D : MonoBehaviour {
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength  = hit.distance;
 
+                if (collisions.climbingSlope) {
+                    velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+                }
+
                 collisions.below = directionY == -1;
                 collisions.above = directionY ==  1;
             }
+        }
+    }
+
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle) {
+        float moveDistance = Mathf.Abs(velocity.x);
+        float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+        if (velocity.y <= climbVelocityY) {
+            velocity.y = climbVelocityY;
+            velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+            collisions.below = true;
+            collisions.climbingSlope = true;
+            collisions.slopeAngle = slopeAngle;
         }
     }
 
@@ -114,9 +152,16 @@ public class Controller_2D : MonoBehaviour {
         public bool above, below;
         public bool left, right;
 
+        public bool climbingSlope;
+        public float slopeAngle, slopeAngleOld;
+
         public void Reset() {
             above = below = false;
-            left = right = false;
+            left  = right = false;
+            climbingSlope = false;
+
+            slopeAngleOld = slopeAngle;
+            slopeAngle = 0;
         }
 
     }
--- Player_Input.cs
+++ Player_Input.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(Controller_2D))]
 public class Player_Input : MonoBehaviour {
    
+    [SerializeField]
     float moveSpeed = 6f;
+    [SerializeField]
+    float accelerationTimeGrounded = .1f;
+    [SerializeField]
     float jumpHeight = 4f;
+    [SerializeField]
+    float accelerationTimeAirbourne = .2f;
+    [SerializeField]
     float timeToJumpApex = .4f;
     float gravity;
     float jumpVelocity;
+    float velocityXSmoothing;
+
     Vector3 velocity;
 
     Controller_2D controller_2D;
@@ -18,7 +27,7 @@ public class Player_Input : MonoBehaviour {
         controller_2D = GetComponent<Controller_2D>();
 
         gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        jumpVelocity = gravity * timeToJumpApex;
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
     }
 
     void Update() {
@@ -33,7 +42,8 @@ public class Player_Input : MonoBehaviour {
             velocity.y = jumpVelocity;
         }
 
-        velocity.x = input.x * moveSpeed;
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller_2D.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirbourne);
         velocity.y += gravity * Time.deltaTime;
         controller_2D.Move(velocity * Time.deltaTime);
     }
